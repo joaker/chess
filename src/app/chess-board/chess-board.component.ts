4507621f05f958ae3c20bb3b184a13747b83d55e
John Oakley <jooakley@gmail.com>
2025-05-08 17:06:56 -0500

    use svgs

--- src/app/chess-board/chess-board.component.ts
+++ src/app/chess-board/chess-board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+type Piece = string | null;
+
 @Component({
   selector: 'app-chess-board',
   templateUrl: './chess-board.component.html',
@@ -8,23 +10,28 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
 })
 export class ChessBoardComponent implements OnInit {
-  board: string[][] = [];
+  board: Piece[][] = [];
 
   ngOnInit(): void {
     this.board = [
-      ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-      ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+      ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
       ['', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', ''],
-      ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-      ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
+      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
     ];
   }
 
   getTileColor(row: number, col: number): string {
     return (row + col) % 2 === 0 ? 'white' : 'black';
   }
+
+  getImagePath(piece: Piece): string | null {
+    return piece ? `assets/pieces/${piece}.svg` : null;
+  }
 }
+
 
