96e862fee97b4ca085cc47b1113e2386dd7c168b
John Oakley <jooakley@gmail.com>
2025-05-09 12:29:46 -0500

    full screen + baseurl set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [ChessBoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [{provide: APP_BASE_HREF, useValue: '/chess'}]
 })
 export class AppComponent {
   title = 'chess';
